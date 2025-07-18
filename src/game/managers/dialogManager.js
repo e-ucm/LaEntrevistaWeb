f46266972d73f2e9a783cdf74077d668d0610071
MattCastUCM <mattcast@ucm.es>
2025-07-18 14:21:41 +0200

    Gestión de las traducciones en el dialogManager

--- src/game/managers/dialogManager.js
+++ src/game/managers/dialogManager.js
@@ -2,6 +2,7 @@ import Singleton from "../../framework/utils/singleton.js";
 import NodeReader from "../../framework/dialog/nodeReader.js";
 import EventDispatcher from "../../framework/managers/eventDispatcher.js";
 import DefaultEventNames from "../../framework/utils/eventNames.js";
+import LocalizationManager from "../../framework/managers/localizationManager.js";
 
 export default class DialogManager extends Singleton {
     constructor() {
@@ -9,6 +10,7 @@ export default class DialogManager extends Singleton {
 
         this.currNode = null;
         this.nodeReader = new NodeReader();
+        this.localizationManager = LocalizationManager.getInstance();
         this.dispatcher = EventDispatcher.getInstance();
         this.dispatcher.add(DefaultEventNames.clearNodes, this, () => {
             this.clearNodes();
@@ -32,14 +34,20 @@ export default class DialogManager extends Singleton {
     }
 
     /**
+    * Lee los nodos con el nodeReader y los traduce
     * @param {Phaser.Scene} scene - escena en la que se crea el nodo
     * @param {Object} fullJson - objeto json donde estan los nodos 
     * @param {String} namespace - nombre del archivo de localizacion del que se va a leer 
-    * @param {String} objectName - nombre del objeto en el que esta el dialogo, si es que el json contiene varios dialogos de distintos objetos
-    * @param {Boolean} getObjs - si se quiere devolver el nodo leido como un objeto 
+    * @param {String} objectName - nombre del objeto en el que esta el dialogo, si es que el json contiene varios dialogos de distintos objetos (opcional)
+    * @param {Object} otherOptions - parametros que pasarle a i18n (opcional) 
     * @returns {DialogNode} - nodo raiz de los nodos leidos
     */
-    readNodes(scene, file, namespace, objectName = "", getObjs = true) {
-        return this.nodeReader.readNodes(scene, file, namespace, objectName, getObjs);
+    readNodes(scene, file, namespace, objectName = "", otherOptions = {}) {
+        let nodes = this.nodeReader.readNodes(scene, file, namespace, objectName);
+
+        nodes.forEach((node) => {
+            node.translate(this.localizationManager, namespace, otherOptions);
+        });
+        return nodes.get("root");
     }
 }
