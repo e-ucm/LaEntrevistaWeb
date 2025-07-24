2a66dac8cab91bf453c9bdc25b5d32c525b1e981
pedroleo <pedroleo@ucm.es>
2025-07-24 19:02:40 +0200

    Eliminar cajas de opción

--- src/game/UI/UI.js
+++ src/game/UI/UI.js
@@ -155,7 +155,6 @@ export default class UI extends BaseUI {
                 }
                 else {
                     currentScene.scene.resume();
-                    this.questionBgElements.activate(false);
                 }
                 this.gameManager.ui.time.paused = visible;
                 if (onComplete != null && typeof onComplete === "function") {
@@ -184,6 +183,7 @@ export default class UI extends BaseUI {
                 pauseMenu.disable();
                 makeVisible(false, () => {
                     menuButton.setInteractive();
+                    this.questionBgElements.activate(false);
                     this.gameManager.startMainMenu();
                     this.shutdown();
                 });
@@ -200,7 +200,7 @@ export default class UI extends BaseUI {
                 exitButton.disableInteractive();
                 pauseMenu.disable();
                 makeVisible(false, () => {
-                    menuButton.setInteractive();
+                    exitButton.setInteractive();
                     this.gameManager.startLanguageMenu();
                     this.shutdown();
                 });
